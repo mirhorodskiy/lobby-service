0614a8a92e05afdafd13afd16ad0dcbea558dc63
mirhorodskiy <mirhorodskiy@gmail.com>
2021-09-08 23:03:36 +0300

    Initial commit

---
+++ src/main/java/tech/pm/lobbyservice/LobbyServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.pm.lobbyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LobbyServiceApplication.class, args);
+  }
+
+}
